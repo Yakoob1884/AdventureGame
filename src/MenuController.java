8b144be810fe33e63ebc45d618a1f7bd9a06abd0
Yakoob1884 <vivja.aty@gmail.com>
2025-04-07 13:02:16 +0200

    show intro

--- src/MenuController.java
+++ src/MenuController.java
@@ -11,7 +11,21 @@ public class MenuController {
     }
 
     public void showIntro(){
-
+        System.out.println("\n\t\t====\tHogwarts Challenge\t====");
+        System.out.println("\tIt was a quiet morning at Hogwarts.");
+        System.out.print("Harry Potter sat alone in the Great Hall.");
+        System.out.println(" The sun pierced through the high\nstained glass windows, but the chill in the air had nothing to do with the weather.");
+        System.out.println("Professor Dumbledore stood. His voice echoed like thunder wrapped in silk.");
+        System.out.println("\t\"Mr. Potter… a new darkness rises. It cannot be fought with spells alone. ");
+        System.out.println("You must complete three trials. Three paths. Each will test your courage, wit, and endurance.\"\n");
+        System.out.println("A magical parchment unraveled midair before Harry, glowing with golden letters:\n");
+        System.out.println("1 ✦\tDiagon Alley – a dragon guards the vaults of Gringotts");
+        System.out.println("2 ✦\tHogsmeade – Malfoy awaits, wand drawn, for a cunning duel");
+        System.out.println("3 ✦\tHogwarts – a troll roams the ancient lower tunnels");
+        System.out.println("0\tExit game\n");
+        System.out.println("\t\"Fail, and you lose more than points. You will suffer magical injuries – breaking bones, losing abilities.");
+        System.out.println("Some spells will become impossible. Even everyday tasks… may not be so simple.\"");
+        System.out.println("Harry clenched his wand tighter. 30 points to begin. Three trials to survive.");
     }
 
     public void showEnding(){
